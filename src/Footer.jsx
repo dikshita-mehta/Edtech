e258b5ec0860145604ed697e906c2c3800ca6f94
dikshita <dikshitamehta9@gmail.com>
2022-05-26 21:22:28 +0530

    all added

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,83 @@
 import React from "react";
 import "./Footer.css";
+import edtech from "./images/edtech.jpg";
 
 function Footer() {
-  return <div className="Footer"></div>;
+  return (
+    <div className="footer">
+      <div class="l-footer">
+        <h1>
+          <img src={edtech} alt="" />
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+          atque recusandae in sit sunt molestiae aliquid fugit. Mollitia eaque
+          tempore iure sit nobis? Vitae nemo, optio maiores numquam quis
+          recusandae.
+        </p>
+      </div>
+      <ul class="r-footer">
+        <li>
+          <h2>Social</h2>
+          <ul class="box">
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Twitter</a>
+            </li>
+            <li>
+              <a href="#">Pinterest</a>
+            </li>
+            <li>
+              <a href="#">Dribbble</a>
+            </li>
+          </ul>
+        </li>
+        <li class="features">
+          <h2>Information</h2>
+          <ul class="box h-box">
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Sales</a>
+            </li>
+            <li>
+              <a href="#">Tickets</a>
+            </li>
+            <li>
+              <a href="#">Certifications</a>
+            </li>
+            <li>
+              <a href="#">Customer Service</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h2>Legal</h2>
+          <ul class="box">
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#">Terms of Use</a>
+            </li>
+            <li>
+              <a href="#">Contract</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div class="b-footer">
+        <p>All rights reserved by EVIRT EDTECH LIMITED 2022 </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
+
