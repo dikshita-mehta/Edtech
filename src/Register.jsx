dee4f07177a7e82f748008d11cbd41590484c0c2
Dikshita Mehta <83219841+dikshita-mehta@users.noreply.github.com>
2022-05-28 14:09:37 +0530

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -3,40 +3,23 @@ import "./Register.css";
 
 function Register() {
   return (
-    <div className="Register_back">
-      <div id="login-box">
-        <div className="left">
-          <h1>Sign up</h1>
-
-          <input type="text" name="username" placeholder="Username" />
-          <input type="text" name="email" placeholder="E-mail" />
-          <input type="password" name="password" placeholder="Password" />
-          <input
-            type="password"
-            name="password2"
-            placeholder="Retype password"
-          />
-
-          <input type="submit" name="signup_submit" value="Sign me up" />
+    <div class="container">
+        <div class="left">
+            <div class="header">
+                <h2 class="animation a1">Register</h2>
+                <h4 class="animation a2">Sign Up by entering your details</h4>
+            </div>
+            <div class="form">
+            <input type="text" class="form-field animation a3" placeholder="First Name"/>
+            <input type="text" class="form-field animation a3" placeholder="Contact No."/>
+            <input type="text" class="form-field animation a3" placeholder="Class"/>
+                <input type="email" class="form-field animation a3" placeholder="Email Address"/>
+                    <input type="password" class="form-field animation a4" placeholder="Password"/>
+                        <button class="animation a6">SIGN UP</button>
+            </div>
         </div>
-
-        <div className="right">
-          <span className="loginwith">
-            Sign in with
-            <br />
-            social network
-          </span>
-
-          <button className="social-signin facebook">
-            Log in with facebook
-          </button>
-          <button className="social-signin twitter">Log in with Twitter</button>
-          <button className="social-signin google">Log in with Google+</button>
-        </div>
-        <div className="or">OR</div>
-      </div>
-    </div>
+        <div class="right"></div>
+  </div>
   );
 }
-
 export default Register;
